37abc1e54e68d9284cbfdb9ab1819c7a9eeb4c2f
d1snin <me@d1s.dev>
2023-01-24 18:21:36 +0300

    base implementation

---
+++ src/commonMain/kotlin/com/catppuccin/kotlin/color/BasePaletteColor.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021 Catppuccin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.catppuccin.kotlin.color
+
+import com.catppuccin.kotlin.Color
+import com.catppuccin.kotlin.ColorValues
+import com.catppuccin.kotlin.Palette
+import com.catppuccin.kotlin.base
+import com.catppuccin.kotlin.util.whenPalette
+
+public class BasePaletteColor(palette: Palette) : PaletteColor(base, palette) {
+
+    override val color = whenPalette(
+        latte = Color(ColorValues.BASE_LATTE_HEX),
+        frappe = Color(ColorValues.BASE_FRAPPE_HEX),
+        macchiato = Color(ColorValues.BASE_MACCHIATO_HEX),
+        mocha = Color(ColorValues.BASE_MOCHA_HEX)
+    )
+}
---
+++ src/commonMain/kotlin/com/catppuccin/kotlin/color/PaletteColorFactory.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2021 Catppuccin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.catppuccin.kotlin.color
+
+import com.catppuccin.kotlin.ColorDefinition
+import com.catppuccin.kotlin.Palette
+import com.catppuccin.kotlin.allColors
+
+public fun paletteColorOf(definition: ColorDefinition, palette: Palette): PaletteColor =
+    definition.paletteColorCreator(palette)
+
+public fun paletteColorsOf(definitions: Iterable<ColorDefinition>, palette: Palette): List<PaletteColor> =
+    definitions.map {
+        paletteColorOf(it, palette)
+    }
+
+public infix fun ColorDefinition.of(palette: Palette): PaletteColor =
+    paletteColorOf(this, palette)
+
+public infix fun Iterable<ColorDefinition>.of(palette: Palette): List<PaletteColor> =
+    paletteColorsOf(this, palette)
+
+public val Palette.rosewater: PaletteColor get() = RosewaterPaletteColor(this)
+
+public val Palette.flamingo: PaletteColor get() = FlamingoPaletteColor(this)
+
+public val Palette.pink: PaletteColor get() = PinkPaletteColor(this)
+
+public val Palette.mauve: PaletteColor get() = MauvePaletteColor(this)
+
+public val Palette.red: PaletteColor get() = RedPaletteColor(this)
+
+public val Palette.maroon: PaletteColor get() = MaroonPaletteColor(this)
+
+public val Palette.peach: PaletteColor get() = PeachPaletteColor(this)
+
+public val Palette.yellow: PaletteColor get() = YellowPaletteColor(this)
+
+public val Palette.green: PaletteColor get() = GreenPaletteColor(this)
+
+public val Palette.teal: PaletteColor get() = TealPaletteColor(this)
+
+public val Palette.sky: PaletteColor get() = SkyPaletteColor(this)
+
+public val Palette.sapphire: PaletteColor get() = SapphirePaletteColor(this)
+
+public val Palette.blue: PaletteColor get() = BluePaletteColor(this)
+
+public val Palette.lavender: PaletteColor get() = LavenderPaletteColor(this)
+
+public val Palette.text: PaletteColor get() = TextPaletteColor(this)
+
+public val Palette.subtext0: PaletteColor get() = Subtext0PaletteColor(this)
+
+public val Palette.subtext1: PaletteColor get() = Subtext1PaletteColor(this)
+
+public val Palette.overlay0: PaletteColor get() = Overlay0PaletteColor(this)
+
+public val Palette.overlay1: PaletteColor get() = Overlay1PaletteColor(this)
+
+public val Palette.overlay2: PaletteColor get() = Overlay2PaletteColor(this)
+
+public val Palette.surface0: PaletteColor get() = Surface0PaletteColor(this)
+
+public val Palette.surface1: PaletteColor get() = Surface1PaletteColor(this)
+
+public val Palette.surface2: PaletteColor get() = Surface2PaletteColor(this)
+
+public val Palette.base: PaletteColor get() = BasePaletteColor(this)
+
+public val Palette.mantle: PaletteColor get() = MantlePaletteColor(this)
+
+public val Palette.crust: PaletteColor get() = CrustPaletteColor(this)
+
+public val Palette.colors: List<PaletteColor> get() = paletteColorsOf(allColors, this)
