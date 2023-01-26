4054e02753860dead05b3cd29fc90fa7585f709f
d1snin <me@d1s.dev>
2023-01-26 13:56:31 +0300

    chore: make color value constants typed with typealiases

--- src/commonMain/kotlin/com/catppuccin/kotlin/ColorValue.kt
+++ src/commonMain/kotlin/com/catppuccin/kotlin/ColorValue.kt
@@ -24,159 +24,159 @@ package com.catppuccin.kotlin
 
 internal object ColorValue {
 
-    internal const val ROSEWATER_LABEL = "Rosewater"
-    internal const val ROSEWATER_LATTE_HEX = 0xdc8a78
-    internal const val ROSEWATER_FRAPPE_HEX = 0xf2d5cf
-    internal const val ROSEWATER_MACCHIATO_HEX = 0xf4dbd6
-    internal const val ROSEWATER_MOCHA_HEX = 0xf5e0dc
+    internal const val ROSEWATER_LABEL: ColorLabel = "Rosewater"
+    internal const val ROSEWATER_LATTE_HEX: Hex = 0xdc8a78
+    internal const val ROSEWATER_FRAPPE_HEX: Hex = 0xf2d5cf
+    internal const val ROSEWATER_MACCHIATO_HEX: Hex = 0xf4dbd6
+    internal const val ROSEWATER_MOCHA_HEX: Hex = 0xf5e0dc
 
-    internal const val FLAMINGO_LABEL = "Flamingo"
-    internal const val FLAMINGO_LATTE_HEX = 0xdd7878
-    internal const val FLAMINGO_FRAPPE_HEX = 0xeebebe
-    internal const val FLAMINGO_MACCHIATO_HEX = 0xf0c6c6
-    internal const val FLAMINGO_MOCHA_HEX = 0xf2cdcd
+    internal const val FLAMINGO_LABEL: ColorLabel = "Flamingo"
+    internal const val FLAMINGO_LATTE_HEX: Hex = 0xdd7878
+    internal const val FLAMINGO_FRAPPE_HEX: Hex = 0xeebebe
+    internal const val FLAMINGO_MACCHIATO_HEX: Hex = 0xf0c6c6
+    internal const val FLAMINGO_MOCHA_HEX: Hex = 0xf2cdcd
 
-    internal const val PINK_LABEL = "Pink"
-    internal const val PINK_LATTE_HEX = 0xea76cb
-    internal const val PINK_FRAPPE_HEX = 0xf4b8e4
-    internal const val PINK_MACCHIATO_HEX = 0xf5bde6
-    internal const val PINK_MOCHA_HEX = 0xf5c2e7
+    internal const val PINK_LABEL: ColorLabel = "Pink"
+    internal const val PINK_LATTE_HEX: Hex = 0xea76cb
+    internal const val PINK_FRAPPE_HEX: Hex = 0xf4b8e4
+    internal const val PINK_MACCHIATO_HEX: Hex = 0xf5bde6
+    internal const val PINK_MOCHA_HEX: Hex = 0xf5c2e7
 
-    internal const val MAUVE_LABEL = "Mauve"
-    internal const val MAUVE_LATTE_HEX = 0x8839ef
-    internal const val MAUVE_FRAPPE_HEX = 0xca9ee6
-    internal const val MAUVE_MACCHIATO_HEX = 0xc6a0f6
-    internal const val MAUVE_MOCHA_HEX = 0xcba6f7
+    internal const val MAUVE_LABEL: ColorLabel = "Mauve"
+    internal const val MAUVE_LATTE_HEX: Hex = 0x8839ef
+    internal const val MAUVE_FRAPPE_HEX: Hex = 0xca9ee6
+    internal const val MAUVE_MACCHIATO_HEX: Hex = 0xc6a0f6
+    internal const val MAUVE_MOCHA_HEX: Hex = 0xcba6f7
 
-    internal const val RED_LABEL = "Red"
-    internal const val RED_LATTE_HEX = 0xd20f39
-    internal const val RED_FRAPPE_HEX = 0xe78284
-    internal const val RED_MACCHIATO_HEX = 0xed8796
-    internal const val RED_MOCHA_HEX = 0xf38ba8
+    internal const val RED_LABEL: ColorLabel = "Red"
+    internal const val RED_LATTE_HEX: Hex = 0xd20f39
+    internal const val RED_FRAPPE_HEX: Hex = 0xe78284
+    internal const val RED_MACCHIATO_HEX: Hex = 0xed8796
+    internal const val RED_MOCHA_HEX: Hex = 0xf38ba8
 
-    internal const val MAROON_LABEL = "Maroon"
-    internal const val MAROON_LATTE_HEX = 0xe64553
-    internal const val MAROON_FRAPPE_HEX = 0xea999c
-    internal const val MAROON_MACCHIATO_HEX = 0xee99a0
-    internal const val MAROON_MOCHA_HEX = 0xeba0ac
+    internal const val MAROON_LABEL: ColorLabel = "Maroon"
+    internal const val MAROON_LATTE_HEX: Hex = 0xe64553
+    internal const val MAROON_FRAPPE_HEX: Hex = 0xea999c
+    internal const val MAROON_MACCHIATO_HEX: Hex = 0xee99a0
+    internal const val MAROON_MOCHA_HEX: Hex = 0xeba0ac
 
-    internal const val PEACH_LABEL = "Peach"
-    internal const val PEACH_LATTE_HEX = 0xfe640b
-    internal const val PEACH_FRAPPE_HEX = 0xef9f76
-    internal const val PEACH_MACCHIATO_HEX = 0xf5a97f
-    internal const val PEACH_MOCHA_HEX = 0xfab387
+    internal const val PEACH_LABEL: ColorLabel = "Peach"
+    internal const val PEACH_LATTE_HEX: Hex = 0xfe640b
+    internal const val PEACH_FRAPPE_HEX: Hex = 0xef9f76
+    internal const val PEACH_MACCHIATO_HEX: Hex = 0xf5a97f
+    internal const val PEACH_MOCHA_HEX: Hex = 0xfab387
 
-    internal const val YELLOW_LABEL = "Yellow"
-    internal const val YELLOW_LATTE_HEX = 0xeed49f
-    internal const val YELLOW_FRAPPE_HEX = 0xe5c890
-    internal const val YELLOW_MACCHIATO_HEX = 0xdf8e1d
-    internal const val YELLOW_MOCHA_HEX = 0xf9e2af
+    internal const val YELLOW_LABEL: ColorLabel = "Yellow"
+    internal const val YELLOW_LATTE_HEX: Hex = 0xdf8e1d
+    internal const val YELLOW_FRAPPE_HEX: Hex = 0xe5c890
+    internal const val YELLOW_MACCHIATO_HEX: Hex = 0xeed49f
+    internal const val YELLOW_MOCHA_HEX: Hex = 0xf9e2af
 
-    internal const val GREEN_LABEL = "Green"
-    internal const val GREEN_LATTE_HEX = 0x40a02b
-    internal const val GREEN_FRAPPE_HEX = 0xa6d189
-    internal const val GREEN_MACCHIATO_HEX = 0xa6da95
-    internal const val GREEN_MOCHA_HEX = 0xa6e3a1
+    internal const val GREEN_LABEL: ColorLabel = "Green"
+    internal const val GREEN_LATTE_HEX: Hex = 0x40a02b
+    internal const val GREEN_FRAPPE_HEX: Hex = 0xa6d189
+    internal const val GREEN_MACCHIATO_HEX: Hex = 0xa6da95
+    internal const val GREEN_MOCHA_HEX: Hex = 0xa6e3a1
 
-    internal const val TEAL_LABEL = "Teal"
-    internal const val TEAL_LATTE_HEX = 0x179299
-    internal const val TEAL_FRAPPE_HEX = 0x81c8be
-    internal const val TEAL_MACCHIATO_HEX = 0x8bd5ca
-    internal const val TEAL_MOCHA_HEX = 0x94e2d5
+    internal const val TEAL_LABEL: ColorLabel = "Teal"
+    internal const val TEAL_LATTE_HEX: Hex = 0x179299
+    internal const val TEAL_FRAPPE_HEX: Hex = 0x81c8be
+    internal const val TEAL_MACCHIATO_HEX: Hex = 0x8bd5ca
+    internal const val TEAL_MOCHA_HEX: Hex = 0x94e2d5
 
-    internal const val SKY_LABEL = "Sky"
-    internal const val SKY_LATTE_HEX = 0x04a5e5
-    internal const val SKY_FRAPPE_HEX = 0x99d1db
-    internal const val SKY_MACCHIATO_HEX = 0x91d7e3
-    internal const val SKY_MOCHA_HEX = 0x89dceb
+    internal const val SKY_LABEL: ColorLabel = "Sky"
+    internal const val SKY_LATTE_HEX: Hex = 0x04a5e5
+    internal const val SKY_FRAPPE_HEX: Hex = 0x99d1db
+    internal const val SKY_MACCHIATO_HEX: Hex = 0x91d7e3
+    internal const val SKY_MOCHA_HEX: Hex = 0x89dceb
 
-    internal const val SAPPHIRE_LABEL = "Sapphire"
-    internal const val SAPPHIRE_LATTE_HEX = 0x209fb5
-    internal const val SAPPHIRE_FRAPPE_HEX = 0x85c1dc
-    internal const val SAPPHIRE_MACCHIATO_HEX = 0x7dc4e4
-    internal const val SAPPHIRE_MOCHA_HEX = 0x74c7ec
+    internal const val SAPPHIRE_LABEL: ColorLabel = "Sapphire"
+    internal const val SAPPHIRE_LATTE_HEX: Hex = 0x209fb5
+    internal const val SAPPHIRE_FRAPPE_HEX: Hex = 0x85c1dc
+    internal const val SAPPHIRE_MACCHIATO_HEX: Hex = 0x7dc4e4
+    internal const val SAPPHIRE_MOCHA_HEX: Hex = 0x74c7ec
 
-    internal const val BLUE_LABEL = "Blue"
-    internal const val BLUE_LATTE_HEX = 0x1e66f5
-    internal const val BLUE_FRAPPE_HEX = 0x8caaee
-    internal const val BLUE_MACCHIATO_HEX = 0x8aadf4
-    internal const val BLUE_MOCHA_HEX = 0x89b4fa
+    internal const val BLUE_LABEL: ColorLabel = "Blue"
+    internal const val BLUE_LATTE_HEX: Hex = 0x1e66f5
+    internal const val BLUE_FRAPPE_HEX: Hex = 0x8caaee
+    internal const val BLUE_MACCHIATO_HEX: Hex = 0x8aadf4
+    internal const val BLUE_MOCHA_HEX: Hex = 0x89b4fa
 
-    internal const val LAVENDER_LABEL = "Lavender"
-    internal const val LAVENDER_LATTE_HEX = 0x7287fd
-    internal const val LAVENDER_FRAPPE_HEX = 0xbabbf1
-    internal const val LAVENDER_MACCHIATO_HEX = 0xb7bdf8
-    internal const val LAVENDER_MOCHA_HEX = 0xb4befe
+    internal const val LAVENDER_LABEL: ColorLabel = "Lavender"
+    internal const val LAVENDER_LATTE_HEX: Hex = 0x7287fd
+    internal const val LAVENDER_FRAPPE_HEX: Hex = 0xbabbf1
+    internal const val LAVENDER_MACCHIATO_HEX: Hex = 0xb7bdf8
+    internal const val LAVENDER_MOCHA_HEX: Hex = 0xb4befe
 
-    internal const val TEXT_LABEL = "Text"
-    internal const val TEXT_LATTE_HEX = 0x4c4f69
-    internal const val TEXT_FRAPPE_HEX = 0xc6d0f5
-    internal const val TEXT_MACCHIATO_HEX = 0xcad3f5
-    internal const val TEXT_MOCHA_HEX = 0xcdd6f4
+    internal const val TEXT_LABEL: ColorLabel = "Text"
+    internal const val TEXT_LATTE_HEX: Hex = 0x4c4f69
+    internal const val TEXT_FRAPPE_HEX: Hex = 0xc6d0f5
+    internal const val TEXT_MACCHIATO_HEX: Hex = 0xcad3f5
+    internal const val TEXT_MOCHA_HEX: Hex = 0xcdd6f4
 
-    internal const val SUBTEXT0_LABEL = "Subtext0"
-    internal const val SUBTEXT0_LATTE_HEX = 0x6c6f85
-    internal const val SUBTEXT0_FRAPPE_HEX = 0xa5adce
-    internal const val SUBTEXT0_MACCHIATO_HEX = 0xa5adcb
-    internal const val SUBTEXT0_MOCHA_HEX = 0xa6adc8
+    internal const val SUBTEXT0_LABEL: ColorLabel = "Subtext0"
+    internal const val SUBTEXT0_LATTE_HEX: Hex = 0x6c6f85
+    internal const val SUBTEXT0_FRAPPE_HEX: Hex = 0xa5adce
+    internal const val SUBTEXT0_MACCHIATO_HEX: Hex = 0xa5adcb
+    internal const val SUBTEXT0_MOCHA_HEX: Hex = 0xa6adc8
 
-    internal const val SUBTEXT1_LABEL = "Subtext1"
-    internal const val SUBTEXT1_LATTE_HEX = 0x5c5f77
-    internal const val SUBTEXT1_FRAPPE_HEX = 0xb5bfe2
-    internal const val SUBTEXT1_MACCHIATO_HEX = 0xb8c0e0
-    internal const val SUBTEXT1_MOCHA_HEX = 0xbac2de
+    internal const val SUBTEXT1_LABEL: ColorLabel = "Subtext1"
+    internal const val SUBTEXT1_LATTE_HEX: Hex = 0x5c5f77
+    internal const val SUBTEXT1_FRAPPE_HEX: Hex = 0xb5bfe2
+    internal const val SUBTEXT1_MACCHIATO_HEX: Hex = 0xb8c0e0
+    internal const val SUBTEXT1_MOCHA_HEX: Hex = 0xbac2de
 
-    internal const val OVERLAY0_LABEL = "Overlay0"
-    internal const val OVERLAY0_LATTE_HEX = 0x9ca0b0
-    internal const val OVERLAY0_FRAPPE_HEX = 0x737994
-    internal const val OVERLAY0_MACCHIATO_HEX = 0x6e738d
-    internal const val OVERLAY0_MOCHA_HEX = 0x6c7086
+    internal const val OVERLAY0_LABEL: ColorLabel = "Overlay0"
+    internal const val OVERLAY0_LATTE_HEX: Hex = 0x9ca0b0
+    internal const val OVERLAY0_FRAPPE_HEX: Hex = 0x737994
+    internal const val OVERLAY0_MACCHIATO_HEX: Hex = 0x6e738d
+    internal const val OVERLAY0_MOCHA_HEX: Hex = 0x6c7086
 
-    internal const val OVERLAY1_LABEL = "Overlay1"
-    internal const val OVERLAY1_LATTE_HEX = 0x8c8fa1
-    internal const val OVERLAY1_FRAPPE_HEX = 0x838ba7
-    internal const val OVERLAY1_MACCHIATO_HEX = 0x8087a2
-    internal const val OVERLAY1_MOCHA_HEX = 0x7f849c
+    internal const val OVERLAY1_LABEL: ColorLabel = "Overlay1"
+    internal const val OVERLAY1_LATTE_HEX: Hex = 0x8c8fa1
+    internal const val OVERLAY1_FRAPPE_HEX: Hex = 0x838ba7
+    internal const val OVERLAY1_MACCHIATO_HEX: Hex = 0x8087a2
+    internal const val OVERLAY1_MOCHA_HEX: Hex = 0x7f849c
 
-    internal const val OVERLAY2_LABEL = "Overlay2"
-    internal const val OVERLAY2_LATTE_HEX = 0x7c7f93
-    internal const val OVERLAY2_FRAPPE_HEX = 0x949cbb
-    internal const val OVERLAY2_MACCHIATO_HEX = 0x939ab7
-    internal const val OVERLAY2_MOCHA_HEX = 0x9399b2
+    internal const val OVERLAY2_LABEL: ColorLabel = "Overlay2"
+    internal const val OVERLAY2_LATTE_HEX: Hex = 0x7c7f93
+    internal const val OVERLAY2_FRAPPE_HEX: Hex = 0x949cbb
+    internal const val OVERLAY2_MACCHIATO_HEX: Hex = 0x939ab7
+    internal const val OVERLAY2_MOCHA_HEX: Hex = 0x9399b2
 
-    internal const val SURFACE0_LABEL = "Surface0"
-    internal const val SURFACE0_LATTE_HEX = 0xccd0da
-    internal const val SURFACE0_FRAPPE_HEX = 0x414559
-    internal const val SURFACE0_MACCHIATO_HEX = 0x363a4f
-    internal const val SURFACE0_MOCHA_HEX = 0x313244
+    internal const val SURFACE0_LABEL: ColorLabel = "Surface0"
+    internal const val SURFACE0_LATTE_HEX: Hex = 0xccd0da
+    internal const val SURFACE0_FRAPPE_HEX: Hex = 0x414559
+    internal const val SURFACE0_MACCHIATO_HEX: Hex = 0x363a4f
+    internal const val SURFACE0_MOCHA_HEX: Hex = 0x313244
 
-    internal const val SURFACE1_LABEL = "Surface1"
-    internal const val SURFACE1_LATTE_HEX = 0xbcc0cc
-    internal const val SURFACE1_FRAPPE_HEX = 0x51576d
-    internal const val SURFACE1_MACCHIATO_HEX = 0x494d64
-    internal const val SURFACE1_MOCHA_HEX = 0x45475a
+    internal const val SURFACE1_LABEL: ColorLabel = "Surface1"
+    internal const val SURFACE1_LATTE_HEX: Hex = 0xbcc0cc
+    internal const val SURFACE1_FRAPPE_HEX: Hex = 0x51576d
+    internal const val SURFACE1_MACCHIATO_HEX: Hex = 0x494d64
+    internal const val SURFACE1_MOCHA_HEX: Hex = 0x45475a
 
-    internal const val SURFACE2_LABEL = "Surface2"
-    internal const val SURFACE2_LATTE_HEX = 0xacb0be
-    internal const val SURFACE2_FRAPPE_HEX = 0x626880
-    internal const val SURFACE2_MACCHIATO_HEX = 0x5b6078
-    internal const val SURFACE2_MOCHA_HEX = 0x585b70
+    internal const val SURFACE2_LABEL: ColorLabel = "Surface2"
+    internal const val SURFACE2_LATTE_HEX: Hex = 0xacb0be
+    internal const val SURFACE2_FRAPPE_HEX: Hex = 0x626880
+    internal const val SURFACE2_MACCHIATO_HEX: Hex = 0x5b6078
+    internal const val SURFACE2_MOCHA_HEX: Hex = 0x585b70
 
-    internal const val BASE_LABEL = "Base"
-    internal const val BASE_LATTE_HEX = 0xeff1f5
-    internal const val BASE_FRAPPE_HEX = 0x303446
-    internal const val BASE_MACCHIATO_HEX = 0x24273a
-    internal const val BASE_MOCHA_HEX = 0x1e1e2e
+    internal const val BASE_LABEL: ColorLabel = "Base"
+    internal const val BASE_LATTE_HEX: Hex = 0xeff1f5
+    internal const val BASE_FRAPPE_HEX: Hex = 0x303446
+    internal const val BASE_MACCHIATO_HEX: Hex = 0x24273a
+    internal const val BASE_MOCHA_HEX: Hex = 0x1e1e2e
 
-    internal const val MANTLE_LABEL = "Mantle"
-    internal const val MANTLE_LATTE_HEX = 0xe6e9ef
-    internal const val MANTLE_FRAPPE_HEX = 0x292c3c
-    internal const val MANTLE_MACCHIATO_HEX = 0x1e2030
-    internal const val MANTLE_MOCHA_HEX = 0x181825
+    internal const val MANTLE_LABEL: ColorLabel = "Mantle"
+    internal const val MANTLE_LATTE_HEX: Hex = 0xe6e9ef
+    internal const val MANTLE_FRAPPE_HEX: Hex = 0x292c3c
+    internal const val MANTLE_MACCHIATO_HEX: Hex = 0x1e2030
+    internal const val MANTLE_MOCHA_HEX: Hex = 0x181825
 
-    internal const val CRUST_LABEL = "Crust"
-    internal const val CRUST_LATTE_HEX = 0xdce0e8
-    internal const val CRUST_FRAPPE_HEX = 0x232634
-    internal const val CRUST_MACCHIATO_HEX = 0x181926
-    internal const val CRUST_MOCHA_HEX = 0x11111b
+    internal const val CRUST_LABEL: ColorLabel = "Crust"
+    internal const val CRUST_LATTE_HEX: Hex = 0xdce0e8
+    internal const val CRUST_FRAPPE_HEX: Hex = 0x232634
+    internal const val CRUST_MACCHIATO_HEX: Hex = 0x181926
+    internal const val CRUST_MOCHA_HEX: Hex = 0x11111b
 }
